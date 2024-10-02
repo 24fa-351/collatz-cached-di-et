f7cd1ed70bd12ed4a32e4c820959a0e42cd2fcca
Daniel <63619914+di-et@users.noreply.github.com>
2024-10-01 23:43:50 -0700

    ignore modifications

--- collatz.c
+++ collatz.c
@@ -3,32 +3,6 @@
 #include "collatz.h"
 
 
-/*
-int collatz(int N) {
-    int count = 0;
-    while (N != 1) {
-        if (N % 2 == 0) {
-            N /= 2;
-        } else {
-            N = 3 * N + 1;
-        }
-        count++;
-    }
-    return count;
-}
-
-int collatz_r(int N) {
-    if (N == 1) {
-        return 0;
-    }
-    if (N % 2 == 0) {
-        return 1 + collatz_r(N/2);
-    } else {
-        return 1 + collatz_r(3 * N + 1);
-    }
-    return 0;
-}
-*/
 
 int main(int argc, char *argv[]) {
     int N;
